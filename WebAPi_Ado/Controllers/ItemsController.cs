8492bcb611565aaefc72df6d3b2cdcecd19531b2
Intern3pk <Intern3pk@DESKTOP-QPK-01>
2023-08-18 17:47:19 +0500

    Working on controller or Program.cs file and Implement Logging

--- WebAPi_Ado/Controllers/ItemsController.cs
+++ WebAPi_Ado/Controllers/ItemsController.cs
@@ -11,53 +11,82 @@ namespace WebAPi_Ado.Controllers
     public class ItemsController : ControllerBase
     {
         private readonly DataAccessItems _dataAccessItems;
+        private readonly ILogger<ItemsController> _logger;
 
-        public ItemsController(IConfiguration configuration)
+        public ItemsController(IConfiguration configuration, ILogger<ItemsController> logger)
         {
             _dataAccessItems = new DataAccessItems(configuration);
+            _logger = logger;
         }
 
         // GET: api/items
         [HttpGet]
         public ActionResult<List<Item>> GetItems()
         {
-            List<Item> items = _dataAccessItems.GetItems();
-            return Ok(items);
+            _logger.LogInformation("Getting items...");
+            try {
+                List<Item> items = _dataAccessItems.GetItems();
+                _logger.LogInformation($"Retrieved {items.Count} items.");
+                return Ok(items);
+            }
+            catch (Exception ex) 
+            {
+                _logger.LogError(ex, "Error while getting items.");
+                return StatusCode(500, "An error occurred while fetching items.");
+            }
+           
         }
 
         // GET: api/items/{id}
         [HttpGet("{id}")]
         public ActionResult<Item> GetItem(int id)
         {
-            Item item = _dataAccessItems.GetItems(id);
-
-            if (item == null)
+            _logger.LogInformation("Getting items by ID");
+            try
             {
-                return NotFound();
-            }
+                Item item = _dataAccessItems.GetItems(id);
 
-            return Ok(item);
+                if (item == null)
+                {
+                    _logger.LogWarning($"Item with ID {id} not found.");
+                    return NotFound();
+                }
+
+                _logger.LogInformation($"Retrieved item: {JsonConvert.SerializeObject(item)}");
+
+                return Ok(item);
+            }
+            catch(Exception ex) 
+            {
+                _logger.LogError(ex, $"Error while getting item with ID: {id}");
+                return StatusCode(500, "An error occurred while fetching the item.");
+            }
+            
         }
 
         // POST: api/items
         [HttpPost]
-        public  ActionResult item(Item newItem)
+        public  ActionResult PostItem(Item newItem)
         {
+            _logger.LogInformation("Adding a new item.");
             try
             {
                 int rowsAffected = _dataAccessItems.AddItem(newItem);
 
                 if (rowsAffected > 0)
                 {
+                    _logger.LogInformation($"Added item: {JsonConvert.SerializeObject(newItem)}");
                     return Ok("Item added successfully.");
                 }
                 else
                 {
+                    _logger.LogWarning("Failed to add item.");
                     return BadRequest("Failed to add item.");
                 }
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Error while adding a new item.");
                 return StatusCode(500, ex.Message);
             }
         }
@@ -66,21 +95,25 @@ namespace WebAPi_Ado.Controllers
         [HttpPut]
         public ActionResult Result(Item updatedItem)
         {
+            _logger.LogInformation("Updating Items");
             try
             {
                 int rowsAffected = _dataAccessItems.UpdateItems(updatedItem);
 
                 if (rowsAffected > 0)
                 {
+                    _logger.LogInformation($"Item Updated {JsonConvert.SerializeObject(updatedItem)}");
                     return Ok("Item updated successfully.");
                 }
                 else
                 {
+                    _logger.LogWarning("Failed to Update Item.");
                     return NotFound("Item not found.");
                 }
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Error while Updating a new Item.");
                 return StatusCode(500, ex.Message);
             }
         }
@@ -89,21 +122,26 @@ namespace WebAPi_Ado.Controllers
         [HttpDelete("{id}")]
         public  ActionResult DeleteItem(int id)
         {
+            _logger.LogInformation("Deleting Item...");
+            int rowsAffected = _dataAccessItems.DeleteItem(id);
             try
             {
-                int rowsAffected = _dataAccessItems.DeleteItem(id);
+                
 
                 if (rowsAffected > 0)
                 {
+                    _logger.LogInformation($"Item {id} Deleted Successfully");
                     return Ok("Item deleted successfully.");
                 }
                 else
                 {
+                    _logger.LogWarning("Failed to Delete Item");
                     return NotFound("Item not found.");
                 }
             }
             catch (Exception ex)
             {
+                _logger.LogError(ex, "Error while Updating a new Item.");
                 return StatusCode(500, ex.Message);
             }
         }
--- WebAPi_Ado/Program.cs
+++ WebAPi_Ado/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using WebAPi_Ado.DataAccessLayer;
 using FluentValidation;
 using FluentValidation.AspNetCore;
 using System.Reflection;
+using Serilog;
+using Serilog.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +16,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddControllers().AddFluentValidation(c => c.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly()));
+
+var logFilePath = Path.Combine(builder.Environment.ContentRootPath, "Log/application.log");
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .WriteTo.File(logFilePath, rollingInterval: RollingInterval.Day) // Specify file path and rolling interval
+    .CreateLogger();
+
+builder.Logging.AddProvider(new SerilogLoggerProvider(Log.Logger));
+
+
 
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -18,19 +35,34 @@ builder.Services.AddScoped<DataAccessItems>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+//app.UseSwagger();
+//app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-
 app.MapControllers();
 
+////Logging at the end of the pipeline
+//app.Use(async (context, next) =>
+//{
+//    // Example of logging a request
+//    app.Logger.LogInformation($"Received request: {context.Request.Method} {context.Request.Path}");
+
+//    await next();
+
+//    // Example of logging a response
+//    app.Logger.LogInformation($"Sent response: {context.Response.StatusCode}");
+//});
+
 app.Run();
+
